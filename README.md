ac7cc1ecda13049a51300ec409a152426e8bd4a5
MiguelAValdesG <mvaldes988@gmail.com>
2020-06-27 15:33:49 -0500

    Added user model

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-"# CookiesAndSessions" 
+# CookiesAndSessions
 
-## References
-Appdelante
-Introducción a Cookies y Sesiones con Node.js 
+## References:
+### Appdelante
+* Introducción a Cookies y Sesiones con Node.js 
 https://www.youtube.com/playlist?list=PLImOJ2OqvvkBEJBCOL_LMaUdLoi8SkAUI
+
+* Autenticación de usuarios con Node.js
+https://www.youtube.com/playlist?list=PLImOJ2OqvvkDYRy2oy4ECs8CVsCOppfA_
--- server.js
+++ server.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+
+const User = require('./models/user')
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/auth';
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URL, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.connection.on('error', (err) => {
+  throw err;
+  process.exit(1);
+})
+
+// const user = new User({
+//   email: 'mvaldes@vainilladev.com',
+//   name: 'MiguelAValdesG',
+//   password: '1234567'
+// })
+
+// user.save()
+//   .then(() => {
+//     console.log('User saved'); 
+//   })
+//   .catch((error) => {
+//     console.log(error);    
+//   })
 
 app.use(session({
   secret: 'THIS IS A SECRET. HERE WE CAN PUT A VARIABLE OF ENVIRONMENT',
